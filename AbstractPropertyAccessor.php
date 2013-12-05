4f8448178724169d2fe96ef7a2479b3bb664a7aa
k-holy <k.holy74@gmail.com>
2013-12-05 19:20:25 +0900

    __clone() を実装, __sleep() の不具合を修正

--- AbstractPropertyAccessor.php
+++ AbstractPropertyAccessor.php
@@ -17,64 +17,6 @@ abstract class AbstractPropertyAccessor implements \ArrayAccess, \IteratorAggreg
 {
 
 	/**
-	 * ArrayAccess::offsetSet()
-	 *
-	 * @param mixed
-	 * @param mixed
-	 */
-	public function offsetSet($name, $value)
-	{
-		if (!property_exists($this, $name)) {
-			throw new \InvalidArgumentException(
-				sprintf('The attribute "%s" does not exists.', $name)
-			);
-		}
-		return $this->{$name} = $value;
-	}
-
-	/**
-	 * ArrayAccess::offsetGet()
-	 *
-	 * @param mixed
-	 * @return mixed
-	 */
-	public function offsetGet($name)
-	{
-		if (!property_exists($this, $name)) {
-			throw new \InvalidArgumentException(
-				sprintf('The attribute "%s" does not exists.', $name)
-			);
-		}
-		return $this->{$name};
-	}
-
-	/**
-	 * ArrayAccess::offsetUnset()
-	 *
-	 * @param mixed
-	 */
-	public function offsetUnset($name)
-	{
-		if (!property_exists($this, $name)) {
-			throw new \InvalidArgumentException(
-				sprintf('The attribute "%s" does not exists.', $name)
-			);
-		}
-		$this->{$name} = null;
-	}
-
-	/**
-	 * ArrayAccess::offsetExists()
-	 *
-	 * @param mixed
-	 * @return bool
-	 */
-	public function offsetExists($name)
-	{
-		return (property_exists($this, $name) && !is_null($this->{$name}));
-	}
-
-	/**
 	 * 引数なしの場合は全てのプロパティを配列で返します。
 	 * 引数ありの場合は全てのプロパティを引数の配列からセットして$thisを返します。
 	 *
@@ -90,14 +32,134 @@ abstract class AbstractPropertyAccessor implements \ArrayAccess, \IteratorAggreg
 			$properties = func_get_arg(0);
 			if (!is_array($properties) && !($properties instanceof \Traversable)) {
 				throw new \InvalidArgumentException(
-					'The properties is not Array and not Traversable.');
+					sprintf('The properties is not an Array and not Traversable. type:%s',
+						is_object($properties)
+							? get_class($properties)
+							: gettype($properties)
+					)
+				);
 			}
 			foreach ($properties as $name => $value) {
-				$this->offsetSet($name, $value);
+				if (!property_exists($this, $name)) {
+					throw new \InvalidArgumentException(
+						sprintf('The property "%s" does not exists.', $name)
+					);
+				}
+				$this->{$name} = $value;
 			}
 			return $this;
 		}
 		throw new \InvalidArgumentException('Invalid argument count.');
+	}
+
+	/**
+	 * ArrayAccess::offsetExists()
+	 *
+	 * @param string プロパティ名
+	 * @return bool
+	 */
+	public function offsetExists($name)
+	{
+		return (property_exists($this, $name) && $this->{$name} !== null);
+	}
+
+	/**
+	 * __isset
+	 *
+	 * @param string プロパティ名
+	 * @return bool
+	 */
+	public function __isset($name)
+	{
+		return $this->offsetExists($name);
+	}
+
+	/**
+	 * ArrayAccess::offsetGet()
+	 *
+	 * @param string プロパティ名
+	 * @return mixed
+	 */
+	public function offsetGet($name)
+	{
+		if (!property_exists($this, $name)) {
+			throw new \InvalidArgumentException(
+				sprintf('The property "%s" does not exists.', $name)
+			);
+		}
+		return $this->{$name};
+	}
+
+	/**
+	 * __get
+	 *
+	 * @param string プロパティ名
+	 */
+	public function __get($name)
+	{
+		return $this->offsetGet($name);
+	}
+
+	/**
+	 * ArrayAccess::offsetSet()
+	 *
+	 * @param mixed
+	 * @param mixed
+	 */
+	public function offsetSet($name, $value)
+	{
+		if (!property_exists($this, $name)) {
+			throw new \InvalidArgumentException(
+				sprintf('The property "%s" does not exists.', $name)
+			);
+		}
+		return $this->{$name} = $value;
+	}
+
+	/**
+	 * __set
+	 *
+	 * @param string プロパティ名
+	 * @param mixed プロパティ値
+	 */
+	public function __set($name, $value)
+	{
+		$this->offsetSet($name, $value);
+	}
+
+	/**
+	 * ArrayAccess::offsetUnset()
+	 *
+	 * @param mixed
+	 */
+	public function offsetUnset($name)
+	{
+		if (!property_exists($this, $name)) {
+			throw new \InvalidArgumentException(
+				sprintf('The property "%s" does not exists.', $name)
+			);
+		}
+		$this->{$name} = null;
+	}
+
+	/**
+	 * __unset
+	 *
+	 * @param string プロパティ名
+	 */
+	public function __unset($name)
+	{
+		$this->offsetUnset($name);
+	}
+
+	/**
+	 * __toString
+	 *
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return var_export($this->properties(), true);
 	}
 
 	/**
@@ -111,74 +173,25 @@ abstract class AbstractPropertyAccessor implements \ArrayAccess, \IteratorAggreg
 	}
 
 	/**
-	 * 配列に変換して返します。
+	 * __sleep
 	 *
 	 * @return array
 	 */
-	public function toArray()
-	{
-		return $this->properties();
-	}
-
-	/**
-	 * magic setter
-	 *
-	 * @param string プロパティ名
-	 * @param mixed プロパティ値
-	 */
-	public function __set($name, $value)
-	{
-		$this->offsetSet($name, $value);
-	}
-
-	/**
-	 * magic getter
-	 *
-	 * @param string プロパティ名
-	 */
-	public function __get($name)
-	{
-		return $this->offsetGet($name);
-	}
-
-	/**
-	 * magic isset
-	 *
-	 * @param string プロパティ名
-	 * @return bool
-	 */
-	public function __isset($name)
-	{
-		return $this->offsetExists($name);
-	}
-
-	/**
-	 * magic unset
-	 *
-	 * @param string プロパティ名
-	 */
-	public function __unset($name)
-	{
-		$this->offsetUnset($name);
-	}
-
-	/**
-	 * __toString
-	 */
-	public function __toString()
-	{
-		return var_export($this->properties(), true);
-	}
-
-	/**
-	 * __sleep
-	 *
-	 * @param void
-	 * @return void
-	 */
 	public function __sleep()
 	{
-		return array_keys($this->properties(), true);
+		return array_keys($this->properties());
+	}
+
+	/**
+	 * __clone
+	 */
+	public function __clone()
+	{
+		foreach ($this->properties() as $name => $value) {
+			if (is_object($value)) {
+				$this->{$name} = clone $value;
+			}
+		}
 	}
 
 	/**
@@ -187,11 +200,21 @@ abstract class AbstractPropertyAccessor implements \ArrayAccess, \IteratorAggreg
 	 * @param array
 	 * @return object
 	 */
-	public static function __set_state($attributes)
+	public static function __set_state($properties)
 	{
 		$instance = new static();
-		$instance->properties($attributes);
+		$instance->properties($properties);
 		return $instance;
+	}
+
+	/**
+	 * 配列に変換して返します。
+	 *
+	 * @return array
+	 */
+	public function toArray()
+	{
+		return $this->properties();
 	}
 
 }
